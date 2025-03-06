add62144f3ff0e4eae779f1e1fd60a1cfb72c343
Nicole Saibert Carneiro da Silva <147657767+nicolecarneiro@users.noreply.github.com>
2025-03-06 13:30:01 -0300

    g

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 
 document.addEventListener('DOMContentLoaded', function(){
+    const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade');
+const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade');       
+botaoDeAcessibilidade.addEventListener('click', function (){
+    botaoDeAcessibilidade.classList.toggle('rotacao-botao');
+    opcoesDeAcessibilidade.classList.toggle('apresenta-lista')
+   })
+ 
+
+
+
+
     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
     const diminuiFonteBotao = document.getElementById('diminuir-fonte');
 
@@ -17,10 +28,3 @@ document.addEventListener('DOMContentLoaded', function(){
     });
 
 });
-const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade')
-const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade')
-botaoDeAcessibilidade.addEventListener('click', function (){
-    botaoDeAcessibilidade.classList.toggle('rotacao-botao');
-    opcoesDeAcessibilidade.classList.toggle('apresenta-lista')
-   })
- 
